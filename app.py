1bbec291fb7655130e6231286895cf35e94463b1
Ann Denise Tadeo <150144454+andnstadeo@users.noreply.github.com>
2024-01-28 15:52:27 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/menu')
+def profile():
+    return render_template('menu.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/order')
+def order():
+    return render_template('order.html')
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
